5568b45b2941e733e8ad7f8b0bcc0ad99d6bd4d8
Jessica Pope <jessica.pope.co@gmail.com>
2019-11-18 21:29:43 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM has been fully loaded')
-  console.table(gifts)
+  console.log(gifts)
+  let giftsUl = document.querySelector('.gift-list')
+
+  const appendGifts = (gifts) =>{
+    gifts.map(gift =>{
+      const giftImg = document.createElement('img')
+      giftImg.src = gift.image
+      const giftName = document.createElement('li')
+      giftName.innerText = gift.name
+      giftsUl.append(giftName, giftImg)
+    })
+  }
+  appendGifts(gifts)
+  let searchInput = document.getElementById('filter-input')
+  let filter = searchInput.value
+  console.log(filter)
 })
